58d6540d36cde1f797e0a22a36e2dfeaacd15f32
ergin.tuerk <e.tuerk@jo-media.net>
2021-07-23 10:35:05 +0200

    Add serialize class WebClientRequest

---
+++ JsonInterface/WebClientRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebsocketMQTTBridge.JsonInterface
+{
+  [Serializable]
+  class WebClientRequest
+  {
+    string command;
+  }
+}
