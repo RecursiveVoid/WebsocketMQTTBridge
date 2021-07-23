5a95c935853baee878abc97fad2f152aad89dc12
ergin.tuerk <e.tuerk@jo-media.net>
2021-07-23 11:13:50 +0200

    Change class name DefaultWebSocketBehaviour to WebSocketBehaviour

---
+++ Websocket/Behaviours/WebSocketBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using WebSocketSharp;
+using WebSocketSharp.Server;
+using Newtonsoft.Json;
+using WebsocketMQTTBridge.JsonInterface;
+using WebsocketMQTTBridge.Util;
+
+namespace WebsocketMQTTBridge.Websocket.Behaviours
+{
+  class WebSocketBehaviour: WebSocketBehavior
+  {
+    protected override void OnClose(CloseEventArgs e)
+    {
+      base.OnClose(e);
+    }
+
+    protected override void OnError(ErrorEventArgs e)
+    {
+      base.OnError(e);
+    }
+    protected override void OnOpen()
+    {
+      base.OnOpen();
+    }
+
+    protected override void OnMessage(MessageEventArgs e)
+    {
+      var webRequestExtractor = new WebRequestExtractor();
+      var request = e.Data;
+      var extractedRequest = webRequestExtractor.extract(request);
+      ConsoleWritter.writeInfo(extractedRequest.ToString(), "RECIEVED FROM CLIENT:");
+      /*
+var msg = e.Data == "BALUS"
+    ? "I've been balused already..."
+    : "I'm not available now.";
+Send(msg);
+*/
+      // TODO extract the command and see what it is
+    }
+  }
+}
