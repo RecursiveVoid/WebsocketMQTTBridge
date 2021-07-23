e6193fda75829eff3801470d8412c4b882e92dc9
ergin.tuerk <e.tuerk@jo-media.net>
2021-07-23 10:35:28 +0200

    Add WebClientConnectionRequest which inherits the WebClientRequest

---
+++ JsonInterface/WebClientConnectionRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebsocketMQTTBridge.JsonInterface
+{
+  [Serializable]
+  class WebClientConnectionRequest: WebClientRequest
+  {
+    string ip;
+    int port;
+    string clientId;
+  }
+}
