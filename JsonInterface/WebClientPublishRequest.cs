3f5cdbb1ae49d961dafcaea8fbf05878ea511315
ergin.tuerk <e.tuerk@jo-media.net>
2021-07-26 17:04:49 +0200

    Add WebClientPublishRequest

---
+++ JsonInterface/WebClientPublishRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebsocketMQTTBridge.JsonInterface
+{
+  [Serializable]
+  class WebClientPublishRequest: WebClientRequest
+  {
+    public string topic;
+    public string device;
+    public string method;
+    public int id;
+
+
+    public override string ToString()
+    {
+      return "command: " + @command + ", topic: " + @topic + ", device: " + @device + ", method: " + @method + "id: " + id;
+    }
+  }
+}
