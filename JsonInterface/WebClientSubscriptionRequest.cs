6afe315544ee1409ff4b917dc685b9770e7799d9
ergin.tuerk <e.tuerk@jo-media.net>
2021-07-26 17:04:59 +0200

    Add WebClientSubscriptionRequest

---
+++ JsonInterface/WebClientSubscriptionRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebsocketMQTTBridge.JsonInterface
+{
+  [Serializable]
+  class WebClientSubscriptionRequest: WebClientRequest
+  {
+    public string[] topics;
+
+    public override string ToString()
+    {
+
+      string requestTopics = "";
+      foreach(string topic in topics)
+      {
+        requestTopics += " ," + topic;
+      }
+      return "command: " + @command + "Topics:[ " + @requestTopics + " ]";
+    }
+  }
+}
