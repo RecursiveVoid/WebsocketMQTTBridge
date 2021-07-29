70491a51e190e5d68a57abc46a7cf19a187bc9a4
ergin.tuerk <e.tuerk@jo-media.net>
2021-07-29 15:23:10 +0200

    Add WebclientRequest Type

---
+++ Websocket/WebclientRequestType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebsocketMQTTBridge.Websocket
+{
+  enum WebclientRequestType
+  {
+    WEBSOCKET,
+    MQTT,
+    INVALID,
+  }
+}
