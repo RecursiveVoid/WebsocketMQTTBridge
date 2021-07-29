854f967bfedcb61dcdb6c67fad13223efd9780f3
ergin.tuerk <e.tuerk@jo-media.net>
2021-07-29 15:23:32 +0200

    Add WebsocketResponseCreator

---
+++ Websocket/WebsocketResponseCreator.cs
@@ -0,0 +1,37 @@
+﻿using WebsocketMQTTBridge.JsonInterface.WebClientRequestInterface;
+
+namespace WebsocketMQTTBridge.Websocket
+{
+  class WebsocketResponseCreator
+  {
+    public WebsocketResponseCreator()
+    {
+      // TODO
+    }
+
+    public string createConnectionOkResponse()
+    {
+      return "{\"command\": \"ok\", \"message\": \"socketConnectionOk\"}";
+    }
+
+    public string createConnectionRejectedResponse()
+    {
+      return "{\"command\": \"error\", \"message\": \"socketConnectionRejected.\"}";
+    }
+
+    public string createConnectionError()
+    {
+      return "{\"command\": \"error\", \"message\": \"connectionError.\"}";
+    }
+
+    public string createInvalidRequestResponse()
+    {
+      return "{\"command\": \"error\", \"message\": \"invalidResponse.\"}";
+    }
+
+    public string createMqttOkResponse(WebClientRequest request)
+    {
+      return "{\"command\": \"ok\", \"message\": \"" + request.command + "\"}";
+    }
+  }
+}
