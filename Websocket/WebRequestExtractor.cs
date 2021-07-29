885e8564f39dd3a295b4a2fd2a0b1863d5610516
ergin.tuerk <e.tuerk@jo-media.net>
2021-07-29 15:23:59 +0200

    Add getRequestType to WebRequestExtractor

--- Websocket/WebRequestExtractor.cs
+++ Websocket/WebRequestExtractor.cs
@@ -12,6 +12,49 @@ namespace WebsocketMQTTBridge.Websocket
       // TODO
     }
 
+    public WebclientRequestType getRequestType(string request)
+    {
+      try
+      {
+        BaseJsonInterface baseRequest = JsonConvert.DeserializeObject<BaseJsonInterface>(request);
+        if (baseRequest != null)
+        {
+          WebClientRequest webClientRequest = JsonConvert.DeserializeObject<WebClientRequest>(request);
+          if (webClientRequest != null)
+          {
+            var command = webClientRequest.command.ToLower();
+            switch (command)
+            {
+              case "connect":
+                return WebclientRequestType.MQTT;
+              case "subscribe":
+                return WebclientRequestType.MQTT;
+              case "publish":
+                return WebclientRequestType.MQTT;
+              default:
+                // TODO change it to error
+                return WebclientRequestType.INVALID;
+            }
+          }
+          else
+          {
+            // TODO change it to error
+            return WebclientRequestType.INVALID;
+          }
+        }
+        else
+        {
+          // TODO change it to error
+          return WebclientRequestType.INVALID;
+        }
+
+      }
+      catch (Exception e)
+      {
+        return WebclientRequestType.INVALID;
+      }
+    }
+
     public BaseJsonInterface extract(string request)
     {
       try 
