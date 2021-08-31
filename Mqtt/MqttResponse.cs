756365692ee99a1e061a454f891e09f98c9b1e57
ergin.tuerk <e.tuerk@jo-media.net>
2021-08-31 15:08:39 +0200

    Add MqttResponse type for defining mqtt connection

---
+++ Mqtt/MqttResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebsocketMQTTBridge.Mqtt
+{
+  public enum MqttResponse
+  {
+    CONNECTION_ERROR,
+    DISCONNECT,
+    RESPONSE_OK,
+    INVALID_RESPONSE,
+    CONNECTION_REJECTED,
+    ALREADY_CONNECTED,
+    MQTTCLIENT_UNAVAILABLE,
+    ALREADY_UNSUBSCRIBED,
+    UNSUBSCRIBED,
+    SUBSCRIBED,
+    ALREADY_SUBSCRIBED,
+    CONNECTION_OK,
+    CRITICAL_ERROR,
+    NO_SUBSCRIBTIONS,
+  }
+}
